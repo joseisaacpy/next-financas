7b77a582bb52562245f2cfae4bd6e6116e0dc183
José Isaac <joseisaacnascimento@gmail.com>
2025-12-04 15:46:23 -0300

    feat(layout): add sonner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "@/components/Footer";
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body className={`${poppins.className} flex flex-col min-h-screen`}>
         <main className="flex-1">{children}</main>
+        <Toaster position="top-center" />
         <Footer />
       </body>
     </html>
