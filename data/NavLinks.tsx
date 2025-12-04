0e2f85d8084c22e816751bd6218edaf95dc24eb4
José Isaac <joseisaacnascimento@gmail.com>
2025-12-04 14:28:37 -0300

    feat(nav-links): adicionar icons e type nos links de navegação

--- data/NavLinks.tsx
+++ data/NavLinks.tsx
@@ -1,34 +1,54 @@
-export const itemsNav = [
+import {
+  Home,
+  BarChart,
+  Calendar,
+  CreditCard,
+  DollarSign,
+  PieChart,
+} from "lucide-react";
+
+type NavItem = {
+  name: string;
+  description: string;
+  href: string;
+  icon: React.ReactNode;
+};
+
+export const navItems: NavItem[] = [
   {
     name: "Início",
     description: "Acesse o painel de controle.",
     href: "/",
+    icon: <Home />,
   },
   {
     name: "Gastos",
     description: "Veja e gerencie todos os seus gastos registrados.",
     href: "/gastos",
+    icon: <CreditCard />,
   },
   {
     name: "Novo Gasto",
     description: "Adicione uma nova despesa ao seu controle.",
-    href: "/novo-gasto",
+    href: "/gastos/novo",
+    icon: <DollarSign />,
   },
   {
     name: "Nova Receita",
     description: "Registre um novo ganho ou entrada de dinheiro.",
-    href: "/nova-receita",
+    href: "/receitas/nova",
+    icon: <Calendar />,
   },
   {
     name: "Receitas",
     description: "Acompanhe todas as suas entradas de renda.",
     href: "/receitas",
+    icon: <BarChart />,
   },
   {
     name: "Resumo",
     description: "Visualize um balanço geral das suas finanças.",
     href: "/dashboard",
+    icon: <PieChart />,
   },
 ];
-
-export default itemsNav;
