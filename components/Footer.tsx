754950bd27dbee708d0c411bacf2a12fdefe6e2a
José Isaac <joseisaacnascimento@gmail.com>
2025-12-03 21:55:52 -0300

    feat(footer): criar footer simples

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="w-full px-8 py-4 bg-black flex items-center justify-center">
+      <p className="font-bold text-center text-lg text-white">
+        Next-Finanças - Todos os direitos reservados
+      </p>
+    </footer>
+  );
+}
