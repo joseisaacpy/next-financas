9189c01171e37bb5e3ff514dccc89afd2daaf996
José Isaac <joseisaacnascimento@gmail.com>
2025-12-04 15:50:45 -0300

    style(footer): diminuir tamanho da fonte

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="w-full px-8 py-4 bg-black flex items-center justify-center">
-      <p className="font-bold text-center text-lg text-white">
+      <p className="font-bold text-center text-base text-white">
         Next-Finanças - Todos os direitos reservados
       </p>
     </footer>
