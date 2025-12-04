1cfcd85a3c8b089b61db95949954208747514bfb
José Isaac <joseisaacnascimento@gmail.com>
2025-12-04 20:33:31 -0300

    feat(route): criar rota GET de gastos

--- app/api/gastos/route.ts
+++ app/api/gastos/route.ts
@@ -1,1 +1,16 @@
+import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const gastos = await prisma.gasto.findMany();
+    // valida se as gastos foram encontradas
+    if (!gastos) return NextResponse.json({ error: "gastos não encontradas" });
+    // se nenhuma receita for encontrada, retorna um array vazio
+    if (gastos.length === 0) return NextResponse.json([]);
+    return NextResponse.json(gastos);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Erro ao buscar gastos" });
+  }
+}
