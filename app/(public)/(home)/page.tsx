670d2a49e57f4ab69893dca081e9941a2b5fb9ee
José Isaac <joseisaacnascimento@gmail.com>
2025-12-04 14:29:38 -0300

    feat(home): criar página de navegação - fazer filtro nos links para remover links "/"

--- app/(public)/(home)/page.tsx
+++ app/(public)/(home)/page.tsx
@@ -1,7 +1,34 @@
+import { navItems } from "@/data/NavLinks";
+import Link from "next/link";
+
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Painel de Controle",
+  description: "Painel de Controle",
+};
+
 export default function Home() {
+  // retornar apenas links que são diferente de "/", pois ele é o link para o painel de controle
+  const navFilter = navItems.filter((item) => item.href !== "/");
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
+    <section className="section">
+      {/* texto */}
+      <div className="space-y-2 mb-2">
+        <h1 className="title-primary">Bem-vindo ao Painel de Navegação</h1>
+        <p className="paragraph">
+          Selecione uma das opções abaixo para navegar pelas ferramentas.
+        </p>
+      </div>
+      {/* links */}
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {navFilter.map((item) => (
+          <li key={item.name}>
+            <Link href={item.href} className="link-menu">
+              {item.icon} {item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
