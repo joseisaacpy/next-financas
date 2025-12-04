6ecce05154ffe7fe84905bf64c10339e74d2e72a
José Isaac <joseisaacnascimento@gmail.com>
2025-12-03 21:56:27 -0300

    feat(layout): importar footer e adicionar props flex no layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={poppins.className}>{children}</body>
+      <body className={`${poppins.className} flex flex-col min-h-screen`}>
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
