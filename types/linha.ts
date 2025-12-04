6e35b6625893602539d2ce51d8938e05d09a8c34
José Isaac <joseisaacnascimento@gmail.com>
2025-12-04 20:30:20 -0300

    fix(type): corrigir type de linha - deixar semelhante ao model do prisma

--- types/linha.ts
+++ types/linha.ts
@@ -1,8 +1,11 @@
+import { Decimal } from "@prisma/client/runtime/client";
+import { CategoriaGasto, CategoriaReceita } from "@/lib/generated/prisma/enums";
+
 export type LinhaTabela = {
   id: number;
   titulo: string;
-  descricao: string;
-  valor: number;
-  categoria: string;
+  descricao: string | null;
+  valor: Decimal;
+  categoria: CategoriaGasto | CategoriaReceita;
   criadoEm: Date;
 };
