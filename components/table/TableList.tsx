90e6add539a4b24d59446c4cd6bcd8a2418a2d9f
José Isaac <joseisaacnascimento@gmail.com>
2025-12-04 21:20:24 -0300

    feat(table-list): adicionar linha de informação quando não houver dados

--- components/table/TableList.tsx
+++ components/table/TableList.tsx
@@ -1,10 +1,19 @@
 import RowTable from "./TableRow";
-import { TableBody } from "@/components/ui/table";
+import { TableBody, TableCell, TableRow } from "@/components/ui/table";
 import type { LinhaTabela } from "@/types/linha";
 
 export default function TableList({ dados }: { dados: LinhaTabela[] }) {
   return (
     <TableBody>
+      {/* se não tiver dados, renderize uma linha informando que não há dados */}
+      {dados.length === 0 && (
+        <TableRow>
+          <TableCell colSpan={6} className="text-center">
+            Nenhum resultado
+          </TableCell>
+        </TableRow>
+      )}
+      {/* se houver dados, renderize-os */}
       {dados.map((item) => (
         <RowTable key={item.id} {...item} />
       ))}
