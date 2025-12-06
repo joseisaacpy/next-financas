398a4b4a6d7f9232be036360e37404bb6264695a
José Isaac <joseisaacnascimento@gmail.com>
2025-12-06 16:15:56 -0300

    feat(route): criar rota GET por id de categoria

---
+++ app/api/categorias/[id]/route.ts
@@ -0,0 +1,36 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
+
+// GET - busca uma categoria por id
+export async function GET(
+  request: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) {
+  try {
+    // pega o id
+    const { id } = await context.params;
+    // busca a categoria
+    const categoria = await prisma.categoria.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    // valida se a categoria foi encontrada
+    if (!categoria)
+      return NextResponse.json(
+        { sucess: false, error: "Categoria não encontrada" },
+        { status: 404 }
+      );
+    // retorna a categoria
+    return NextResponse.json({
+      sucess: true,
+      data: categoria,
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({
+      sucess: false,
+      error: "Erro ao buscar categoria",
+    });
+  }
+}
