f78a812685a4589d12e173181e8660d87e9bcca7
José Isaac <joseisaacnascimento@gmail.com>
2025-12-06 13:40:31 -0300

    fix: remover componentes de linhas de tabelas

--- app/(private)/receitas/page.tsx
+++ app/(private)/receitas/page.tsx
@@ -1,16 +1,12 @@
 import { Table } from "@/components/ui/table";
 import HeaderTable from "@/components/table/HeaderTable";
-import TableList from "@/components/table/TableList";
-import { prisma } from "@/lib/prisma";
 
 export default async function Receitas() {
-  const receitas = await prisma.receita.findMany();
   return (
     <section>
       <h1 className="title-primary">Receitas</h1>
       <Table>
         <HeaderTable />
-        <TableList dados={receitas} />
       </Table>
     </section>
   );
