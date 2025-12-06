3897a31d4908744a8901c8c34cdd34cb3f8deb26
José Isaac <joseisaacnascimento@gmail.com>
2025-12-06 10:45:28 -0300

    feat(route): criar rota GET por id de gastos

--- app/api/gastos/[id]/route.ts
+++ app/api/gastos/[id]/route.ts
@@ -1,6 +1,27 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
+// GET
+export async function GET(
+  request: Request,
+  context: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await context.params;
+    const gasto = await prisma.gasto.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (!gasto) return NextResponse.json({ error: "Gasto não encontrado" });
+    return NextResponse.json(gasto);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Erro ao buscar gasto" });
+  }
+}
+
+// DELETE
 export async function DELETE(
   request: Request,
   context: { params: Promise<{ id: string }> }
@@ -18,4 +39,3 @@ export async function DELETE(
     return NextResponse.json({ error: "Erro ao deletar gasto" });
   }
 }
-
