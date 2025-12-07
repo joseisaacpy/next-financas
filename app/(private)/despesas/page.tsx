0925f221b452735a05604e75d43ceed815b23233
José Isaac <joseisaacnascimento@gmail.com>
2025-12-07 16:07:51 -0300

    feat: faz a listagem de despesas e receitas em suas respectivas rotas

--- app/(private)/despesas/page.tsx
+++ app/(private)/despesas/page.tsx
@@ -1,12 +1,26 @@
 import { Table } from "@/components/ui/table";
 import HeaderTable from "@/components/table/HeaderTable";
+import TableList from "@/components/table/TableList";
+import { prisma } from "@/lib/prisma";
 
 export default async function Gastos() {
+  const despesas = await prisma.transacao.findMany({
+    where: {
+      tipo: "DESPESA",
+    },
+    orderBy: {
+      criadoEm: "desc",
+    },
+    include: {
+      categoria: true,
+    },
+  });
   return (
     <section>
       <h1 className="title-primary">Despesas</h1>
       <Table>
         <HeaderTable />
+        <TableList dados={despesas} />
       </Table>
     </section>
   );
--- app/(private)/receitas/page.tsx
+++ app/(private)/receitas/page.tsx
@@ -1,12 +1,26 @@
 import { Table } from "@/components/ui/table";
 import HeaderTable from "@/components/table/HeaderTable";
+import TableList from "@/components/table/TableList";
+import { prisma } from "@/lib/prisma";
 
 export default async function Receitas() {
+  const receitas = await prisma.transacao.findMany({
+    where: {
+      tipo: "RECEITA",
+    },
+    orderBy: {
+      criadoEm: "desc",
+    },
+    include: {
+      categoria: true,
+    },
+  });
   return (
     <section>
       <h1 className="title-primary">Receitas</h1>
       <Table>
         <HeaderTable />
+        <TableList dados={receitas} />
       </Table>
     </section>
   );
