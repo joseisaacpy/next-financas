840616b57b152c2bb8466e80f6f9b74620fd3dee
José Isaac <joseisaacnascimento@gmail.com>
2025-12-07 15:41:36 -0300

    refactor(form-cadastro): refatorar estrutura do formulário e inputs - corrigir envio de resposta pra api correta - unificar formulário para cadastro de receita e despesa

--- components/FormCadastro.tsx
+++ components/FormCadastro.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
@@ -14,83 +14,91 @@ import {
 import { Label } from "@/components/ui/label";
 import { toast } from "sonner";
 import type { Categoria } from "@/types/categoria";
-import { useCategorias } from "@/hooks/categorias";
+import { FormData } from "@/types/formData";
 
-export default function FormCadastro({ tipo }: Categoria) {
-  // faz um filtro de categorias com base no tipo do formulário
-  const categorias = useCategorias(tipo);
+export default function FormCadastro() {
+  // estado para armazenar as categorias
+  const [categorias, setCategoria] = useState<Categoria[]>([]);
 
   // objeto do form
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<FormData>({
     titulo: "",
     descricao: "",
     valor: 0,
+    tipo: "",
+    categoriaId: "",
     data: "",
-    categoria: "",
   });
+
   //   função para enviar o formulário
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     // evitar o comportamento padrão do formulário
     e.preventDefault();
 
     // verificar se todos os campos obrigatórios foram preenchidos
-    if (!formData.titulo || !formData.data || !formData.categoria) {
+    if (
+      !formData.titulo ||
+      !formData.valor ||
+      !formData.categoriaId ||
+      !formData.tipo ||
+      !formData.data
+    ) {
       toast.error("Preencha todos os campos");
       return;
     }
+    console.table(formData);
     try {
       // enviar os dados para a API
-      const response = await fetch(
-        `/api/${tipo === "gasto" ? "gastos" : "receitas"}`,
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(formData),
-        }
-      );
+      const response = await fetch(`/api/transacoes`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
       // verificar se a resposta foi bem sucedida
       if (!response.ok) {
         throw new Error("Erro ao cadastrar");
       }
-      // obter os dados da resposta
-      const data = await response.json();
 
       // limpar o formulário
       setFormData({
         titulo: "",
         descricao: "",
         valor: 0,
+        tipo: "",
+        categoriaId: "",
         data: "",
-        categoria: "",
       });
 
       // mensagem de sucesso
-      toast.success(
-        `${
-          tipo === "gasto"
-            ? "Gasto cadastrado com sucesso"
-            : "Receita cadastrada com sucesso"
-        }`
-      );
+      toast.success("Cadastro realizado com sucesso!");
     } catch (error) {
       console.error(error);
       // mensagem de erro
-      toast.error(
-        `${
-          tipo === "gasto"
-            ? "Erro ao cadastrar gasto"
-            : "Erro ao cadastrar receita"
-        }`
-      );
+      toast.error("Erro ao realizar o cadastro!");
     }
   };
+
+  // função para buscar as categorias
+  useEffect(() => {
+    async function buscarCategorias() {
+      try {
+        const response = await fetch(`/api/categorias`);
+        const data = await response.json();
+        setCategoria(data.data);
+      } catch (error) {
+        console.error(error);
+        toast.error("Erro ao carregar categorias");
+      }
+    }
+    // chama a função
+    buscarCategorias();
+  }, []);
   return (
     // section
     <section className="section">
       {/* titulo */}
-      <h1 className="title-primary mb-2">
-        {tipo === "gasto" ? "Novo Gasto" : "Nova Receita"}
-      </h1>
+      <h1 className="title-primary mb-2">Cadastre uma nova transação</h1>
       {/* form */}
       <form onSubmit={handleSubmit} className="form">
         <div className="form-group">
@@ -117,30 +125,50 @@ export default function FormCadastro({ tipo }: Categoria) {
               setFormData({ ...formData, descricao: e.target.value })
             }
           />
-
-          <div className="form-group">
-            <Label htmlFor="valor">Valor:</Label>
-            <Input
-              type="number"
-              step="0.01"
-              id="valor"
-              placeholder="Digite um valor"
-              value={formData.valor}
-              onChange={(e) =>
-                setFormData({ ...formData, valor: parseFloat(e.target.value) })
-              }
-            />
-          </div>
         </div>
 
         <div className="form-group">
+          <Label htmlFor="valor">Valor:</Label>
+          <Input
+            type="number"
+            step="0.01"
+            id="valor"
+            placeholder="Digite um valor"
+            value={formData.valor}
+            onChange={(e) =>
+              setFormData({ ...formData, valor: Number(e.target.value) })
+            }
+          />
+        </div>
+
+        <div className="form-group">
+          <Label htmlFor="tipo">Tipo:</Label>
+          <Select
+            value={formData.tipo}
+            onValueChange={(value: "DESPESA" | "RECEITA") =>
+              setFormData({ ...formData, tipo: value })
+            }
+          >
+            <SelectTrigger aria-label="Selecione um tipo" className="w-full">
+              <SelectValue placeholder="Selecione um tipo" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Tipo</SelectLabel>
+                <SelectItem value="DESPESA">Despesa</SelectItem>
+                <SelectItem value="RECEITA">Receita</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="form-group">
           <Label htmlFor="categoria">Categoria:</Label>
           <Select
-            value={formData.categoria}
+            value={formData.categoriaId}
             onValueChange={(value) =>
               setFormData({
                 ...formData,
-                categoria: value,
+                categoriaId: value,
               })
             }
           >
@@ -154,8 +182,8 @@ export default function FormCadastro({ tipo }: Categoria) {
               <SelectGroup>
                 <SelectLabel>Categorias</SelectLabel>
                 {categorias.map((categoria) => (
-                  <SelectItem key={categoria} value={categoria}>
-                    {categoria}
+                  <SelectItem key={categoria.id} value={String(categoria.id)}>
+                    {categoria.nome}
                   </SelectItem>
                 ))}
               </SelectGroup>
