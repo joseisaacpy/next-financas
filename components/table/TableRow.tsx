bfbdc57ca49a3aa9c5be2b702e078948d5d9d792
José Isaac <joseisaacnascimento@gmail.com>
2025-12-07 16:42:42 -0300

    feat(table-row): usar useRouter para atualizar o componente aṕos operações - ajustar função de deletar - criar variável com data formatada

--- components/table/TableRow.tsx
+++ components/table/TableRow.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useRouter } from "next/navigation";
 import { TableCell, TableRow } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 import type { LinhaTabela } from "@/types/linha";
@@ -8,13 +10,28 @@ export default function RowTable({
   titulo,
   descricao,
   valor,
+  tipo,
   categoria,
   criadoEm,
 }: LinhaTabela) {
+  // cria instancia do router
+  const router = useRouter();
+
   // função para lidar com a exclusão
   const handleDelete = async (id: number) => {
     try {
-      toast.success("Excluido com sucesso");
+      const response = await fetch(`/api/transacoes/${id}`, {
+        method: "DELETE",
+      });
+
+      // verifica se a resposta foi bem sucedida
+      if (!response.ok) {
+        throw new Error("Falha ao excluir");
+      }
+
+      // mensagem de sucesso e atualiza a tabela
+      toast.success("Excluído com successo");
+      router.refresh();
     } catch (error) {
       console.error(error);
       toast.error("Erro ao excluir");
@@ -24,25 +41,53 @@ export default function RowTable({
   // função para lidar com a edição
   const handleEdit = async (id: number) => {
     try {
-      toast.success("Editado com sucesso");
+      const response = await fetch(`/api/transacoes/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          id,
+          titulo,
+          descricao,
+          valor,
+          tipo,
+          categoriaId: categoria.id,
+        }),
+      });
+
+      // verifica se a resposta foi bem sucedida
+      if (!response.ok) {
+        throw new Error("Falha ao editar");
+      }
+      // mensagem de sucesso e atualiza a tabela
+      toast.success("Editado com successo");
+      router.refresh();
     } catch (error) {
       console.error(error);
       toast.error("Erro ao editar");
     }
   };
+
+  // transforma a data em formato brasileiro
+  const dataFormatada = new Date(criadoEm).toLocaleDateString("pt-BR", {
+    timeZone: "UTC",
+  });
+
   return (
     <TableRow>
-      <TableCell className="font-medium">
-        {criadoEm.toLocaleDateString("pt-BR")}
-      </TableCell>
+      <TableCell className="font-medium">{dataFormatada}</TableCell>
       <TableCell className="font-medium">{titulo}</TableCell>
-      <TableCell className="font-medium">{descricao}</TableCell>
-      <TableCell className="font-medium">{valor}</TableCell>
-      <TableCell className="font-medium">{categoria}</TableCell>
+      <TableCell className="font-medium">{descricao || "-"}</TableCell>
+      <TableCell className="font-medium">
+        {Number(valor).toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </TableCell>
+      <TableCell className="font-medium">{categoria.nome}</TableCell>
       <TableCell>
         <div className="flex gap-2">
-          <Button onClick={() => handleDelete}>Excluir</Button>
-          <Button onClick={() => handleEdit}>Editar</Button>
+          <Button onClick={() => handleDelete(id)}>Excluir</Button>
+          <Button onClick={() => handleEdit(id)}>Editar</Button>
         </div>
       </TableCell>
     </TableRow>
