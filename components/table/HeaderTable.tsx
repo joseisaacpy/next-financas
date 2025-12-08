0728e942a6b4a5dc8d988c88929bff4677208407
José Isaac <joseisaacnascimento@gmail.com>
2025-12-07 21:53:37 -0300

    style(header-table): adicionar alinhamento à esquerda nos textos

--- components/table/HeaderTable.tsx
+++ components/table/HeaderTable.tsx
@@ -4,12 +4,12 @@ export default function HeaderTable() {
   return (
     <TableHeader>
       <TableRow>
-        <TableHead>Data</TableHead>
-        <TableHead>Nome</TableHead>
-        <TableHead>Descrição</TableHead>
-        <TableHead>Valor</TableHead>
-        <TableHead>Categoria</TableHead>
-        <TableHead>Ações</TableHead>
+        <TableHead className="text-left">Data</TableHead>
+        <TableHead className="text-left">Nome</TableHead>
+        <TableHead className="text-left">Descrição</TableHead>
+        <TableHead className="text-left">Valor</TableHead>
+        <TableHead className="text-left">Categoria</TableHead>
+        <TableHead className="text-left">Ações</TableHead>
       </TableRow>
     </TableHeader>
   );
