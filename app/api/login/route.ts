b43ed4133bac9e7f54973243ede983551b489479
José Isaac <joseisaacnascimento@gmail.com>
2025-12-08 22:00:36 -0300

    feat(jwt): instalar jsonwebtoken - criar token ao realizar login válido por 2dias

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,13 +1,20 @@
 import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
 export async function POST(request: Request) {
   try {
-    // pega o body
+    // pega o body vindo do request
     const { email, senha } = await request.json();
-    // pega o email e senha no env
+
+    // pega o email, senha e secret
     const emailEnv = process.env.EMAIL;
     const senhaEnv = process.env.PASSWORD_HASH!;
+    const jwtSecret = process.env.JWT_SECRET!;
+
+    // pega o cookie
+    const cookieStore = cookies();
 
     // valida se os dados foram preenchidos
     if (!email || !senha) {
@@ -32,12 +39,21 @@ export async function POST(request: Request) {
         { status: 401 }
       );
     }
-    // retorna o token
+
+    // Gera token JWT
+    const token = jwt.sign({ email }, jwtSecret, { expiresIn: "2d" });
+
+    // Salva cookie
+    (await cookieStore).set("token", token, {
+      httpOnly: true,
+      secure: true,
+      path: "/",
+      maxAge: 60 * 60 * 24 * 2, // 2 dias
+    });
     return NextResponse.json(
       {
         success: true,
         message: "Login realizado com sucesso.",
-        token: "token",
       },
       { status: 200 }
     );
