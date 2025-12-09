8c297f8ccd33245b0ec796d656672a85f6ecc0eb
José Isaac <joseisaacnascimento@gmail.com>
2025-12-09 20:27:30 -0300

    feat(header): criar button responsável por logout do user

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { useState } from "react";
 import { navItems } from "@/data/NavLinks";
-import { Menu, X } from "lucide-react";
+import { LogOut, Menu, X } from "lucide-react";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Close } from "@radix-ui/react-dialog";
+import logout from "@/lib/logout";
 
 export default function Header() {
   // pegar a rota atual
@@ -14,34 +14,23 @@ export default function Header() {
   const [menuMobile, setMenuMobile] = useState<boolean>(false);
   return (
     <header className="bg-gray-800">
-      <nav className="flex justify-between px-4 py-2 md:justify-around items-center">
+      <nav className="flex justify-between px-4 py-4 md:justify-around items-center">
         {/* logo */}
         <h1 className="text-2xl font-bold text-white">NextFinanças</h1>
-
-        {/* botão para abrir o menu */}
-        <Button
-          className="md:hidden"
-          variant={"ghost"}
-          onClick={() => setMenuMobile(!menuMobile)}
-        >
-          <Menu className="w-6 h-6 text-white" />
-        </Button>
-
         {/* menu mobile */}
         <ul
           className={`${
             menuMobile ? "flex" : "hidden"
-          } absolute top-0 left-0 w-full h-screen flex-col items-center justify-center gap-2 bg-gray-800 text-white`}
+          } absolute top-0 left-0 w-full h-screen flex-col items-center justify-center bg-gray-800 text-white`}
         >
-          {/* botão para abrir o menu */}
+          {/* botão para fechar o menu */}
           <Button
             className="md:hidden"
             variant={"ghost"}
             onClick={() => setMenuMobile(!menuMobile)}
           >
-            <Menu className="w-6 h-6 text-white" />
+            <X className="w-6 h-6 text-white" />
           </Button>
-
           {navItems.map((item) => (
             <li key={item.name}>
               <Link href={item.href}>
@@ -58,7 +47,6 @@ export default function Header() {
             </li>
           ))}
         </ul>
-
         {/* menu desktop */}
         <ul className="hidden md:flex">
           {navItems.map((item) => (
@@ -76,6 +64,25 @@ export default function Header() {
             </li>
           ))}
         </ul>
+        <div>
+          {/* botão de logout */}
+          <Button
+            className="rounded-full"
+            variant={"link"}
+            onClick={() => logout()}
+          >
+            <LogOut className="w-6 h-6 text-red-500" />
+          </Button>
+
+          {/* botão para abrir o menu */}
+          <Button
+            className="md:hidden"
+            variant={"ghost"}
+            onClick={() => setMenuMobile(!menuMobile)}
+          >
+            <Menu className="w-6 h-6 text-white" />
+          </Button>
+        </div>
       </nav>
     </header>
   );
